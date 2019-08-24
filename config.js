e7d69fe7f04b387c1e5ee5c3aa7419eceb19f96b
tienphat <quangphatc3@gmail.com>
2019-08-24 10:21:52 +0700

    Init project

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports={
+  "facebook_api_key"      :     "414256479200407",
+  "facebook_api_secret"   :     "a73fd736a0ae632724d13b1a2026eb7a",
+  "callback_url"          :     "http://localhost:3000/auth/facebook/callback",
+  "use_database"          :     true,
+  "host"                  :     "localhost",
+  "username"              :     "root",
+  "password"              :     "",
+  "database"              :     "login_passport"
+}
